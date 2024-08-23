7363b54061826345882209347c830e7556c6fa92
Praveen851 <rpraveenbabu851@gmail.com>
2024-08-23 18:27:56 +0530

    feat: use rotate component in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "./Icon";
 import Action from "./ActionComponent";
+import RotationAction from "./RotationAction";
 
 export default function Sidebar({ move, rotate }) {
-    const handleMove = () => move(10, 0);
-    const handleRotateLeft = () => rotate(-15);
-    const handleRotateRight = () => rotate(15);
+    const [antiClockWiseDegree, setAntiClockWiseDegree] = useState(15);
+    const [clockWiseDegree, setClockWiseDegree] = useState(15);
+
     return (
         <div className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-200">
             <div className="font-bold"> {"Events"} </div>
@@ -23,22 +24,25 @@ export default function Sidebar({ move, rotate }) {
             <Action bgColor="bg-yellow-500">
                 {"When this sprite clicked"}
             </Action>
+
             <div className="font-bold"> {"Motion"} </div>
-            <Action onClick={handleMove}>{"Move 10 steps "}</Action>
-            <Action onClick={handleRotateLeft}>
-                <>
-                    {"Turn "}
-                    <Icon name="undo" size={15} className="text-white mx-2" />
-                    {"15 degrees"}
-                </>
-            </Action>
-            <Action onClick={handleRotateRight}>
-                <>
-                    {"Turn "}
-                    <Icon name="redo" size={15} className="text-white mx-2" />
-                    {"15 degrees"}
-                </>
-            </Action>
+            <Action onClick={() => move(10, 0)}>{"Move 10 steps "}</Action>
+
+            <RotationAction
+                label="Turn "
+                icon="undo"
+                degree={antiClockWiseDegree}
+                onDegreeChange={setAntiClockWiseDegree}
+                onRotate={(degree) => rotate(-degree)}
+            />
+
+            <RotationAction
+                label="Turn "
+                icon="redo"
+                degree={clockWiseDegree}
+                onDegreeChange={setClockWiseDegree}
+                onRotate={(degree) => rotate(degree)}
+            />
         </div>
     );
 }
