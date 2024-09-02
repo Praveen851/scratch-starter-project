f1a5bdb9277e6330eb352271f9f93dff4e934443
Praveen851 <rpraveenbabu851@gmail.com>
2024-09-02 22:44:22 +0530

    update webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,14 +3,20 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge([
-  common,
-  {
-    mode: "development",
-    devServer: {
-      contentBase: path.join(__dirname, "public"),
-      compress: true,
-      port: 3000,
-      historyApiFallback: true,
+    common,
+    {
+        mode: "development",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            compress: true,
+            port: 3000,
+            historyApiFallback: true,
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".json"],
+            alias: {
+                "react/jsx-runtime": require.resolve("react/jsx-runtime.js"),
+            },
+        },
     },
-  },
 ]);
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,16 +5,22 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 process.env["NODE_ENV"] = "production";
 
 module.exports = merge([
-  common,
-  {
-    mode: "production",
-    optimization: {
-      minimize: true,
-      minimizer: [
-        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-        // `...`,
-        new CssMinimizerPlugin(),
-      ],
+    common,
+    {
+        mode: "production",
+        optimization: {
+            minimize: true,
+            minimizer: [
+                // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+                // `...`,
+                new CssMinimizerPlugin(),
+            ],
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".json"],
+            alias: {
+                "react/jsx-runtime": require.resolve("react/jsx-runtime.js"),
+            },
+        },
     },
-  },
 ]);
