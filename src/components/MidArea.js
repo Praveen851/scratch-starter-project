eac9eb1dd8545c4483582a076a871a473b07b778
Praveen851 <rpraveenbabu851@gmail.com>
2024-09-03 15:29:09 +0530

    feat: implement drag and drop from sidebar to mid area

--- src/components/MidArea.js
+++ src/components/MidArea.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDrop } from "react-dnd";
+import Icon from "./Icon";
+import Action from "./ActionComponent";
 
 export default function MidArea() {
-  return <div className="flex-1 h-full overflow-auto">{"mid area"} </div>;
+    const [board, setBoard] = useState([]);
+    const [, drop] = useDrop(() => ({
+        accept: "block",
+        drop: (item) => {
+            addTaskToBoard(item);
+        },
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver(),
+        }),
+    }));
+    const addTaskToBoard = (id) => {
+        console.log(id);
+
+        setBoard((board) => [...board, id]);
+    };
+
+    const runTask = () => {
+        if (board.length && board[0].id !== 1) {
+            return;
+        }
+        board.forEach((item) => {
+            if (item.onClick) item.onClick();
+        });
+    };
+    return (
+        <div className="flex-1 h-full overflow-auto Board" ref={drop}>
+            <div className="flex flex-row">
+                <div>Mid Area</div>
+                <button className="mx-4 self-start" onClick={runTask}>
+                    <Icon
+                        name="flag"
+                        size={15}
+                        className="text-green-600 mx-2"
+                    />
+                </button>
+            </div>
+            {board.map((item) => (
+                <Action id={item.id} bgColor={item.bgColor}>
+                    {item.children}
+                </Action>
+            ))}
+        </div>
+    );
 }
--- src/components/RotationAction.js
+++ src/components/RotationAction.js
@@ -7,11 +7,12 @@ export default function RotationAction({
     degree,
     onDegreeChange,
     onRotate,
+    id,
 }) {
     const stopPropagation = (e) => e.stopPropagation();
 
     return (
-        <Action onClick={() => onRotate(degree)}>
+        <Action onClick={() => onRotate(degree)} id={id}>
             <div className="flex flex-row items-center bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
                 {label}
                 <Icon name={icon} size={15} className="text-white mx-2" />
